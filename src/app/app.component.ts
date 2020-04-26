b24f67b276cc97f9f213af92f231d652b0008438
mmacdonald1 <melmacd94@gmail.com>
2020-04-26 14:19:50 -0400

    starter code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { createWorker } from 'tesseract.js';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'tessractApp';
+  title = 'tesseract.js-angular-app';
+  ocrResult = 'Recognizing...';
+  constructor(){
+    this.doOCR();
+  }
+  async doOCR() {
+    const worker = createWorker({
+      logger: m => console.log(m),
+    });
+    await worker.load();
+    await worker.loadLanguage('eng');
+    await worker.initialize('eng');
+    const { data: { text } } = await worker.recognize('https://tesseract.projectnaptha.com/img/eng_bw.png');
+    this.ocrResult = text;
+    console.log(text);
+    await worker.terminate();
+  }
 }
